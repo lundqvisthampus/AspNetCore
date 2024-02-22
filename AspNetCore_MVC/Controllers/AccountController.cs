a9754ad8d5ff5da4abcfbc4c1ecb28eacd3d89b2
lundqvisthampus <lundqvisthampus@outlook.com>
2024-02-22 17:45:01 +0100

    Added account-details

--- AspNetCore_MVC/Controllers/AccountController.cs
+++ AspNetCore_MVC/Controllers/AccountController.cs
@@ -1,7 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore_MVC.Models.Views;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCore_MVC.Controllers;
 
 public class AccountController : Controller
 {
+    public IActionResult Index()
+    {
+        var model = new AccountIndexViewModel();
+        ViewData["Title"] = "Account Details";
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult BasicInfo(AccountIndexViewModel model)
+    {
+        return RedirectToAction("Index", "Account");
+    }
+
+    [HttpPost]
+    public IActionResult AddressInfo(AccountIndexViewModel model)
+    {
+        return RedirectToAction("Index", "Account");
+    }
 }
---
+++ AspNetCore_MVC/Models/Views/AccountIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AspNetCore_MVC.Models.Views;
+
+public class AccountIndexViewModel
+{
+    public string Title { get; set; } = "Account Details";
+    public AccountDetailsBasicInfoModel BasicInfo { get; set; } = new AccountDetailsBasicInfoModel() 
+    {
+        ProfileImage = "/images/profile-img.svg",
+        FirstName = "Hampus",
+        LastName = "Lundqvist",
+        Email = "Hampus@domain.com"
+    };
+    public AccountDetailsAddressInfoModel AddressInfo { get; set; } = new AccountDetailsAddressInfoModel();
+}
