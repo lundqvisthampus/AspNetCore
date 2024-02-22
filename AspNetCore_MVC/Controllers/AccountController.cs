281ca46d5123c3fce285e9d9b4f49c438683f6d0
lundqvisthampus <lundqvisthampus@outlook.com>
2024-02-22 15:44:19 +0100

    Account controller added

--- AspNetCore_MVC/Controllers/AccountController.cs
+++ AspNetCore_MVC/Controllers/AccountController.cs
@@ -4,27 +4,4 @@ namespace AspNetCore_MVC.Controllers;
 
 public class AccountController : Controller
 {
-    public IActionResult Index()
-    {
-        ViewData["Title"] = "Profile";
-        return View();
-    }
-
-    public IActionResult SignUp()
-    {
-        ViewData["Title"] = "Sign Up";
-        return View();
-    }
-
-    public IActionResult SignIn()
-    {
-        ViewData["Title"] = "Sign In";
-        return View();
-    }
-
-    public new IActionResult SignOut()
-    {
-        return RedirectToAction("Index", "Home");
-    }
-
 }
