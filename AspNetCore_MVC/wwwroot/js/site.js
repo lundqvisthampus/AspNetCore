a729bbd99168ee237cddf32022dd569332a68c70
lundqvisthampus <lundqvisthampus@outlook.com>
2024-02-25 18:25:59 +0100

    added javascript for mobile-nav menu

--- AspNetCore_MVC/wwwroot/js/site.js
+++ AspNetCore_MVC/wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿
+﻿const toggleMenu = () => {
+    document.getElementById('menu').classList.toggle('hide');
+}
+
+const checkScreenSize = () => {
+    if (window.innerWidth >= 992) {
+        document.getElementById('menu').classList.add('hide');
+    }
+}
+
+window.addEventListener('resize', checkScreenSize);
