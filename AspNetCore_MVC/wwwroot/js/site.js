e54b7f767408fa38b28dbe71dc9a1f1462b37a0b
lundqvisthampus <lundqvisthampus@outlook.com>
2024-03-14 18:26:07 +0100

    Added JS to make sure the header is always the correct color

--- AspNetCore_MVC/wwwroot/js/site.js
+++ AspNetCore_MVC/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿const toggleDarkMode = () => {
+﻿const header = document.querySelector("header")
+const url = window.location.pathname
+
+const toggleDarkMode = () => {
     const body = document.querySelector('body');
     const darkModeEnabled = body.classList.toggle('dark-mode');
     localStorage.setItem('darkModeEnabled', darkModeEnabled);
@@ -8,6 +11,19 @@
         logo.setAttribute("src", "/images/solid.svg");
     } else {
         logo.setAttribute("src", "/images/logotype.svg");
+    }
+
+    if (darkModeEnabled && url == "/Contact") {
+        header.style.backgroundColor = "#0f121e"
+    }
+    else if (darkModeEnabled && url != "/Contact") {
+        header.style.backgroundColor = "#0B0F19"
+    }
+    else if (darkModeEnabled == false && url == "/Contact") {
+        header.style.backgroundColor = "#F3F6FF"
+    }
+    else {
+        header.style.backgroundColor = "#FFFFFF"
     }
 }
 
@@ -19,11 +35,22 @@ window.addEventListener("DOMContentLoaded", () => {
         body.classList.add('dark-mode');
         document.getElementById("darkModeToggle").checked = true;
         document.querySelector("#logotype").setAttribute("src", "/images/solid.svg");
+        if (url === "/Contact") {
+            header.style.backgroundColor = "#0f121e";
+        } else {
+            header.style.backgroundColor = "#0B0F19";
+        }
     }
     else {
         body.classList.remove('dark-mode');
         document.getElementById("darkModeToggle").checked = false;
         document.querySelector("#logotype").setAttribute("src", "/images/logotype.svg");
+        header.style.backgroundColor = "#FFFFFF"
+        if (url === "/Contact") {
+            header.style.backgroundColor = "#F3F6FF";
+        } else {
+            header.style.backgroundColor = "#FFFFFF";
+        }
     }
 })
 
