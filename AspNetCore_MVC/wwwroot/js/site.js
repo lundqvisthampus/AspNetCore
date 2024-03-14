47970b6f55ab8bf899a95df9fb1d8185ebef24de
lundqvisthampus <lundqvisthampus@outlook.com>
2024-03-14 15:33:38 +0100

    Darkmode added

--- AspNetCore_MVC/wwwroot/js/site.js
+++ AspNetCore_MVC/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿const toggleMenu = () => {
+﻿const toggleDarkMode = () => {
+    const body = document.querySelector('body');
+    const darkModeEnabled = body.classList.toggle('dark-mode');
+    localStorage.setItem('darkModeEnabled', darkModeEnabled);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const body = document.querySelector('body');
+    const darkModeEnabled = localStorage.getItem('darkModeEnabled')
+
+    if (darkModeEnabled === "true") {
+        body.classList.add('dark-mode');
+        document.getElementById("darkModeToggle").checked = true;
+    }
+    else {
+        body.classList.remove('dark-mode');
+        document.getElementById("darkModeToggle").checked = false;
+    }
+})
+
+const toggleMenu = () => {
     document.getElementById('menu').classList.toggle('hide');
 }
 
