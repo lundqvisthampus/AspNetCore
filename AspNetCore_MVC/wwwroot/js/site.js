e73ccf003a34839d006930ebb32823d7af8a00a3
lundqvisthampus <lundqvisthampus@outlook.com>
2024-03-14 16:04:11 +0100

    Added localstorage for darkmode with JS, and also made sure the logotype updates when switching theme.

--- AspNetCore_MVC/wwwroot/js/site.js
+++ AspNetCore_MVC/wwwroot/js/site.js
@@ -2,6 +2,13 @@
     const body = document.querySelector('body');
     const darkModeEnabled = body.classList.toggle('dark-mode');
     localStorage.setItem('darkModeEnabled', darkModeEnabled);
+
+    const logo = document.querySelector("#logotype");
+    if (darkModeEnabled) {
+        logo.setAttribute("src", "/images/solid.svg");
+    } else {
+        logo.setAttribute("src", "/images/logotype.svg");
+    }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -11,12 +18,15 @@ window.addEventListener("DOMContentLoaded", () => {
     if (darkModeEnabled === "true") {
         body.classList.add('dark-mode');
         document.getElementById("darkModeToggle").checked = true;
+        document.querySelector("#logotype").setAttribute("src", "/images/solid.svg");
     }
     else {
         body.classList.remove('dark-mode');
         document.getElementById("darkModeToggle").checked = false;
+        document.querySelector("#logotype").setAttribute("src", "/images/logotype.svg");
     }
 })
+
 
 const toggleMenu = () => {
     document.getElementById('menu').classList.toggle('hide');
