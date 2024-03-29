bcdd022f0724f77fc6330e3d2295ed54231259da
lundqvisthampus <lundqvisthampus@outlook.com>
2024-03-29 09:51:51 +0100

    Added filtering on courses page.

--- AspNetCore_MVC/wwwroot/js/coursesSearch.js
+++ AspNetCore_MVC/wwwroot/js/coursesSearch.js
@@ -16,3 +16,56 @@ input.addEventListener("keyup", () => {
         }
     });
 })
+
+
+
+const select = document.querySelector(".select-input")
+
+select.addEventListener("change", () => {
+    const option = select.options[select.selectedIndex].text;
+
+    if (option == "Best seller") {
+
+        courses.forEach(course => {
+            const bestSeller = course.querySelector(".bestSeller");
+            if (bestSeller != null) {
+                course.classList.remove('hide');
+            }
+            else {
+                course.classList.add('hide');
+            }
+        })
+    }
+
+    else if (option == "All categories") {
+        courses.forEach(course => {
+            course.classList.remove('hide');
+        })
+    }
+
+    else if (option == "Reduced price") {
+
+        courses.forEach(course => {
+            const reducedPrice = course.querySelector(".reducedPrice");
+            if (reducedPrice != null) {
+                course.classList.remove('hide');
+            }
+            else {
+                course.classList.add('hide');
+            }
+        })
+    }
+
+    else if (option == "Saved courses") {
+
+        courses.forEach(course => {
+            const savedCourse = course.querySelector("#deleteCourseBtn");
+            if (savedCourse != null) {
+                course.classList.remove('hide');
+            }
+            else {
+                course.classList.add('hide');
+            }
+        })
+    }
+})
